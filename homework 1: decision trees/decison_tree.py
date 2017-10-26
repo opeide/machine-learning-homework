d3c058cf2a7cecaea446dd3e3e615a1499a49f61
opeide <fredrikopeide@gmail.com>
2017-10-26 14:58:47 +0200

    added files

---
+++ homework 1: decision trees/decison_tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 13:52:02 2017
+
+@author: opeide
+"""
+import numpy as np
+
+
+all_data = np.loadtxt('01_homework_dataset.csv', delimiter=',', skiprows=1)
+
+
